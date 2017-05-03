5486ac90e722c866d061d455eb474e50870ec2c8
sravel <sebastien.ravel@cirad.fr>
2017-05-03 09:41:50 +0200

    add soft bamutilsFilter bamutilsRemoveclipping

--- modules/fileConfigurator.pm
+++ modules/fileConfigurator.pm
@@ -105,7 +105,10 @@ our %testParams=    (
         checkFormatVcf => [""],
         checkFormatSamOrBam => [""],
         #fastqUtils
-        checkEncodeByASCIIcontrol => [""]
+        checkEncodeByASCIIcontrol => [""],
+		# bamutils
+		bamutilsFilter => ["-gte MAPQ 30"],
+		bamutilsRemoveclipping => [""]
         );
 
 sub softParams
