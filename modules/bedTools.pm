73a635d5060e20518c1ffe0ff6b785bbae0370f6
Francois Sabot <francois.sabot@ird.fr>
2017-11-17 15:38:16 +0100

    Correcting bedTools.pm

--- modules/bedTools.pm
+++ modules/bedTools.pm
@@ -83,6 +83,8 @@ sub intersectBed {
 		#Picking up the second file
 		my $crossFile = $options;
 		$crossFile =~ s/.*-[a|b] ([A-Za-z0-9\/-_\.~]+).*/$1/;
+		$crossFile = `readlink -f $crossFile`;
+		chomp $crossFile;
 		toolbox::exportLog("file crossed: $crossFile",2);
 		if (&localFormatCheck($crossFile) == 0)
 		{#The file is not a BAM/VCF/GFF/BED file
