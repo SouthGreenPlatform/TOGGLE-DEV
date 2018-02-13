d4a7e0a821d7255f13c776dc9b7c565df23ac117
christine <christine.tranchant@ird.Fr>
2018-02-13 09:38:53 +0100

    Add parsing flagstat file and integrating into stats.tex

--- modules/stats.pm
+++ modules/stats.pm
@@ -99,6 +99,65 @@ sub mapping
      }
 }
 
+sub generateStats
+{
+
+    my ($statDir)=@_;		# get parameters
+	my $fileList = toolbox::readDir($statDir);		# get stat files list
+	
+	my ($texRaw, $texAssembly, $texMapping, $bool);	# different sections of tex stat file
+	$bool=0;
+	
+	#$texMapping = "\\subsection{Mapping}
+	#\\begin{table}[ht]
+	#	\\centering
+#		\\begin{tabular}{l|r|r|r}
+#			Samples & Raw sequences & Mapped sequences & Properly mapped  \\\\\\hline
+#			toto---tata & 001 & Normal \\\\
+#		\\end{tabular}
+#\\end{table}" ;
+
+	my $statTexFile = "stats.tex";
+	open(my $texFh, ">", $statTexFile) or toolbox::exportLog("$0 : open error of $statTexFile .... $!\n",0);
+	## DEBUG toolbox::exportLog("J ouvre $statTexFile ", 1);
+	# Parsing stat files
+     
+    foreach my $file (@{$fileList}) #Copying the final data in the final directory
+	{
+		toolbox::exportLog("Je lis 1 $file", 1);
+		#my ($basicName)=toolbox::extractPath($file);
+		if ($file =~ /\.flagstat.mapping.stat$/)
+		{
+			$bool=1;
+			open (my $fh, "<", $file) or toolbox::exportLog("$0 : open error of $file .... $!\n",0);
+			my ($raw, $mapped, $properly);
+			$raw = $mapped = $properly = 0;
+               
+               ##DEBUG q toolbox::exportLog("Je lis $file", 1);
+			while (my $line = <$fh>)
+			{
+				if ($line =~ /^(\d+)\s+.+in\stotal/) { $raw = $1; 	toolbox::exportLog("Je lis $line - $1 -", 1);}
+				elsif ($line =~ /^(\d+)\s+.+in\sproperly\spaired/) { $properly = $1; }
+				elsif ($line =~ /^(\d+)\s+.+in\smapped\s\(/) { $mapped = $1; }
+			}
+
+			$texMapping .= " & $raw & $mapped $ $properly ";
+			
+			close $fh;
+			
+		}
+	}
+	
+#	$texMapping .= "\\end{tabular}
+#\\end{table}";
+	
+	print $texFh $texMapping;
+	close $texFh;
+	
+	toolbox::exportLog($texMapping,1);
+
+}
+
 
 1;
 
