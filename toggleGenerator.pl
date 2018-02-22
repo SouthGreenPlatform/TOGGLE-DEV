d55ede2b2894028b01efcf4155c3f2f00027b74d
Francois Sabot <francois.sabot@ird.fr>
2018-02-22 15:48:04 +0100

    Correcting toggleGenerator info from scheduler

--- toggleGenerator.pl
+++ toggleGenerator.pl
@@ -638,7 +638,7 @@ if ($orderBefore1000)
                 toolbox::exportLog("------>".$basicName."\n",1);
 
                 # Moving stat file into stat directory instead of intermediateDir
-                if ($basicName =~ /\.stat$/)
+                if ($basicName =~ /\.tat$/)
                 {
                     my $mvCommand="mv $file $statDir/$basicName && rm -f $file";
                     toolbox::run($mvCommand,"noprint");
