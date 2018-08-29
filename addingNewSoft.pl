9815154a620fb6736314cb3bec1cc07cf0b03b10
Julie Orjuela <julie.orjuela@ird.fr>
2018-08-29 10:36:29 +0200

    adding test block part in simplifier integration

--- addingNewSoft.pl
+++ addingNewSoft.pl
@@ -412,119 +412,115 @@ else
     my $localLine;
     $localLine.="#!/usr/bin/env perl
 
-    ###################################################################################################################################
-    #
-    # Copyright 2014-2018 IRD-CIRAD-INRA-ADNid
-    #
-    # This program is free software; you can redistribute it and/or modify
-    # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation; either version 3 of the License, or
-    # (at your option) any later version.
-    #
-    # This program is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    # GNU General Public License for more details.
-    #
-    # You should have received a copy of the GNU General Public License
-    # along with this program; if not, see <http://www.gnu.org/licenses/> or
-    # write to the Free Software Foundation, Inc.,
-    # 51 Franklin Street, Fifth Floor, Boston,
-    # MA 02110-1301, USA.
-    #
-    # You should have received a copy of the CeCILL-C license with this program.
-    #If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
-    #
-    # Intellectual property belongs to IRD, CIRAD and South Green developpement plateform for all versions also for ADNid for v2 and v3 and INRA for v3
-    # Version 1 written by Cecile Monat, Ayite Kougbeadjo, Christine Tranchant, Cedric Farcy, Mawusse Agbessi, Maryline Summo, and Francois Sabot
-    # Version 2 written by Cecile Monat, Christine Tranchant, Cedric Farcy, Enrique Ortega-Abboud, Julie Orjuela-Bouniol, Sebastien Ravel, Souhila Amanzougarene, and Francois Sabot
-    # Version 3 written by Cecile Monat, Christine Tranchant, Laura Helou, Abdoulaye Diallo, Julie Orjuela-Bouniol, Sebastien Ravel, Gautier Sarah, and Francois Sabot
-    #
-    ###################################################################################################################################
-    
-    ######################################################################################################################################
-    ######################################################################################################################################
-    ## COMMON MODULE TEST HEADER
-    ######################################################################################################################################
-    ######################################################################################################################################
-    
-    use strict;
-    use warnings;
-    use Data::Dumper;
-    
-    use Test::More \'no_plan\'; #Number of tests, to modify if new tests implemented. Can be changed as \'no_plan\' instead of tests=>11 .
-    use Test::Deep;
-    
-    # Load localConfig if primary test is successful
-    use_ok(\'localConfig\') or exit;
-    use localConfig;
-    
-    
-    ########################################
-    # Extract automatically tool name and sub name list
-    ########################################
-    my (\$toolName,\$tmp) = split /_/ , \$0;
-    my \$subFile=\$toggle.\"/modules/\".\$toolName.\".pm\";
-    my \@sub = `grep \"^sub\" \$subFile`or die (\"ERROR: \$0 : Cannot extract automatically sub name list by grep command \\n\$!\\n\");
-    
-    
-    ########################################
-    #Automatically module test with use_ok and can_ok
-    ########################################
-    
-    use_ok(\$toolName) or exit;
-    eval \"use \$toolName\";
-    
-    foreach my \$subName (\@sub)
-    {
-        chomp (\$subName);
-        \$subName =~ s/sub //;
-        can_ok(\$toolName,\$subName);
-    }
-    
-    #########################################
-    #Preparing test directory
-    #########################################
-    my \$testDir=\"\$toggle/dataTest/\$toolName\".\"TestModule\";
-    my \$cmd=\"rm -Rf \$testDir ; mkdir -p \$testDir\";
-    system(\$cmd) and die (\"ERROR: \$0 : Cannot execute the test directory \$testDir (\$toolName) with the following cmd \$cmd\\n\$!\\n\");
-    chdir \$testDir or die (\"ERROR: \$0 : Cannot go into the test directory \$testDir (\$toolName) with the chdir cmd \\n\$!\\n\");
-    
-    
-    #########################################
-    #Creating log file
-    #########################################
-    my \$logFile=\$toolName.\"_log.o\";
-    my \$errorFile=\$toolName.\"_log.e\";
-    system(\"touch \$testDir/\$logFile \$testDir/\$errorFile\") and die \"\\nERROR: \$0 : cannot create the log files \$logFile and \$errorFile: \$!\\nExiting...\\n\";
-    
-    ######################################################################################################################################
-    ######################################################################################################################################
-    
-         
-     
-    ##########################################
-    ### input output Options
-    ##########################################
-    
-    my \%optionsHachees = ();                # Hash containing informations
-    my \$optionHachees = \\%optionsHachees;   # Ref of the hash
+###################################################################################################################################
+#
+# Copyright 2014-2018 IRD-CIRAD-INRA-ADNid
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# You should have received a copy of the CeCILL-C license with this program.
+#If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
+#
+# Intellectual property belongs to IRD, CIRAD and South Green developpement plateform for all versions also for ADNid for v2 and v3 and INRA for v3
+# Version 1 written by Cecile Monat, Ayite Kougbeadjo, Christine Tranchant, Cedric Farcy, Mawusse Agbessi, Maryline Summo, and Francois Sabot
+# Version 2 written by Cecile Monat, Christine Tranchant, Cedric Farcy, Enrique Ortega-Abboud, Julie Orjuela-Bouniol, Sebastien Ravel, Souhila Amanzougarene, and Francois Sabot
+# Version 3 written by Cecile Monat, Christine Tranchant, Laura Helou, Abdoulaye Diallo, Julie Orjuela-Bouniol, Sebastien Ravel, Gautier Sarah, and Francois Sabot
+#
+###################################################################################################################################
+## COMMON MODULE TEST HEADER
+######################################################################################################################################
 
-    ##########################################
-    ##### $module::$function 
-    ##########################################
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Test::More \'no_plan\'; #Number of tests, to modify if new tests implemented. Can be changed as \'no_plan\' instead of tests=>11 .
+use Test::Deep;
+
+# Load localConfig if primary test is successful
+use_ok(\'localConfig\') or exit;
+use localConfig;
+
+
+########################################
+# Extract automatically tool name and sub name list
+########################################
+my (\$toolName,\$tmp) = split /_/ , \$0;
+my \$subFile=\$toggle.\"/modules/\".\$toolName.\".pm\";
+my \@sub = `grep \"^sub\" \$subFile`or die (\"ERROR: \$0 : Cannot extract automatically sub name list by grep command \\n\$!\\n\");
+
+
+########################################
+#Automatically module test with use_ok and can_ok
+########################################
+
+use_ok(\$toolName) or exit;
+eval \"use \$toolName\";
+
+foreach my \$subName (\@sub)
+{
+    chomp (\$subName);
+    \$subName =~ s/sub //;
+    can_ok(\$toolName,\$subName);
+}
+
+#########################################
+#Preparing test directory
+#########################################
+my \$testDir=\"\$toggle/dataTest/\$toolName\".\"TestModule\";
+my \$cmd=\"rm -Rf \$testDir ; mkdir -p \$testDir\";
+system(\$cmd) and die (\"ERROR: \$0 : Cannot execute the test directory \$testDir (\$toolName) with the following cmd \$cmd\\n\$!\\n\");
+chdir \$testDir or die (\"ERROR: \$0 : Cannot go into the test directory \$testDir (\$toolName) with the chdir cmd \\n\$!\\n\");
+
+
+#########################################
+#Creating log file
+#########################################
+my \$logFile=\$toolName.\"_log.o\";
+my \$errorFile=\$toolName.\"_log.e\";
+system(\"touch \$testDir/\$logFile \$testDir/\$errorFile\") and die \"\\nERROR: \$0 : cannot create the log files \$logFile and \$errorFile: \$!\\nExiting...\\n\";
+
+######################################################################################################################################
+######################################################################################################################################
+
      
-     
-    
-    is($module::$function(FILEIN,FILEOUT,\$optionHachees),1,'$module::$function  - Test for $function running');
-    
-    # expected output test
-    my \$observedOutput = \`ls\`;
-    my \@observedOutput = split /\\n/,\$observedOutput;
-    my \@expectedOutput = (\'FILEOUT\',\'".$module."_log.e\',\'".$module."_log.o\');
-    is_deeply(\\\@observedOutput,\\\@expectedOutput,\'$module::$function - output list\');
-    
-    ################ TODO add test for output content
+ 
+##########################################
+### input output Options
+##########################################
+
+my \%optionsHachees = ();                # Hash containing informations
+my \$optionHachees = \\%optionsHachees;   # Ref of the hash
+
+##########################################
+##### $module::$function 
+##########################################
+ 
+ 
+
+is($module::$function(FILEIN,FILEOUT,\$optionHachees),1,'$module::$function  - Test for $function running');
+
+# expected output test
+my \$observedOutput = \`ls\`;
+my \@observedOutput = split /\\n/,\$observedOutput;
+my \@expectedOutput = (\'FILEOUT\',\'".$module."_log.e\',\'".$module."_log.o\');
+is_deeply(\\\@observedOutput,\\\@expectedOutput,\'$module::$function - output list\');
+
+################ TODO add test for output content
     ";
     
     print $fhModule $localLine;
@@ -544,87 +540,81 @@ else
     my $localLine;
     $localLine.="#!/usr/bin/env perl
 
-    ###################################################################################################################################
-    #
-    # Copyright 2014-2018 IRD-CIRAD-INRA-ADNid
-    #
-    # This program is free software; you can redistribute it and/or modify
-    # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation; either version 3 of the License, or
-    # (at your option) any later version.
-    #
-    # This program is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    # GNU General Public License for more details.
-    #
-    # You should have received a copy of the GNU General Public License
-    # along with this program; if not, see <http://www.gnu.org/licenses/> or
-    # write to the Free Software Foundation, Inc.,
-    # 51 Franklin Street, Fifth Floor, Boston,
-    # MA 02110-1301, USA.
-    #
-    # You should have received a copy of the CeCILL-C license with this program.
-    #If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
-    #
-    # Intellectual property belongs to IRD, CIRAD and South Green developpement plateform for all versions also for ADNid for v2 and v3 and INRA for v3
-    # Version 1 written by Cecile Monat, Ayite Kougbeadjo, Christine Tranchant, Cedric Farcy, Mawusse Agbessi, Maryline Summo, and Francois Sabot
-    # Version 2 written by Cecile Monat, Christine Tranchant, Cedric Farcy, Enrique Ortega-Abboud, Julie Orjuela-Bouniol, Sebastien Ravel, Souhila Amanzougarene, and Francois Sabot
-    # Version 3 written by Cecile Monat, Christine Tranchant, Laura Helou, Abdoulaye Diallo, Julie Orjuela-Bouniol, Sebastien Ravel, Gautier Sarah, and Francois Sabot
-    #
-    ###################################################################################################################################
+###################################################################################################################################
+#
+# Copyright 2014-2018 IRD-CIRAD-INRA-ADNid
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# You should have received a copy of the CeCILL-C license with this program.
+#If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
+#
+# Intellectual property belongs to IRD, CIRAD and South Green developpement plateform for all versions also for ADNid for v2 and v3 and INRA for v3
+# Version 1 written by Cecile Monat, Ayite Kougbeadjo, Christine Tranchant, Cedric Farcy, Mawusse Agbessi, Maryline Summo, and Francois Sabot
+# Version 2 written by Cecile Monat, Christine Tranchant, Cedric Farcy, Enrique Ortega-Abboud, Julie Orjuela-Bouniol, Sebastien Ravel, Souhila Amanzougarene, and Francois Sabot
+# Version 3 written by Cecile Monat, Christine Tranchant, Laura Helou, Abdoulaye Diallo, Julie Orjuela-Bouniol, Sebastien Ravel, Gautier Sarah, and Francois Sabot
+#
+###################################################################################################################################
+
+use strict;
+use warnings;
+use Test::More 'no_plan';
+use Test::Deep;
+use fileConfigurator;
+use localConfig;
+use Data::Dumper;    
     
-    ######################################################################################################################################
-    ######################################################################################################################################
-    ## COMMON MODULE TEST HEADER
-    ######################################################################################################################################
-    ######################################################################################################################################
-    
-    use strict;
-    use warnings;
-    use Test::More 'no_plan';
-    use Test::Deep;
-    use fileConfigurator;
-    use localConfig;
-    use Data::Dumper;    
-        
-    #####################
-    ## PATH for data test
-    #####################
-    
-    # references files
-    my \$dataRefIrigin = \"\$toggle/data/Bank/referenceIrigin.fasta\";
-    # input file
-    my \$dataFastq=\"\$toggle/data/testData/fastq/pairedTwoIndividusIrigin\";
-    
-    
-    print \"\\n\\n#################################################\\n\";
-    print \"#### TEST $module \\n\";
-    print \"#################################################\\n\";
-    
-    # Remove files and directory created by previous test
-    my \$testingDir=\"\$toggle/dataTest/$module-noSGE-Blocks\";
-    my \$cleaningCmd=\"rm -Rf \$testingDir\";
-    system (\$cleaningCmd) and die (\"ERROR: \$0 : Cannot remove the previous test directory with the command \$cleaningCmd \\n\$!\\n\");
-    
-    #Creating config file for this test
-    my \@listSoft = (\"$module\");
-    fileConfigurator::createFileConf(\\\@listSoft,\"blockTestConfig.txt\");
-    
-    my \$runCmd = \"toggleGenerator.pl -c blockTestConfig.txt -d \".\$dataFastq.\" -r \".\$dataRefIrigin.\" -o \".\$testingDir;
-    print \"\\n### Toggle running : \$runCmd\\n\";
-    system(\"\$runCmd\") and die \"#### ERROR : Can't run TOGGLE for $module\";
-    
-    # check final results
-    
-    # expected output content
-    my \$observedOutput = \`ls \$testingDir/finalResults\`;
-    my \@observedOutput = split /\\n/,\$observedOutput;
-    my \@expectedOutput = (\'FILE.EXTENTION\',\'FILE.EXTENTION\');
-    
-    is_deeply(\\\@observedOutput,\\\@expectedOutput,\'toggleGenerator - Two FILES (no SGE) $module::$function file list \');
-    
-    # expected output value TO DO
+#####################
+## PATH for data test
+#####################
+
+# references files
+my \$dataRefIrigin = \"\$toggle/data/Bank/referenceIrigin.fasta\";
+# input file
+my \$dataFastq=\"\$toggle/data/testData/fastq/pairedTwoIndividusIrigin\";
+
+
+print \"\\n\\n#################################################\\n\";
+print \"#### TEST $module \\n\";
+print \"#################################################\\n\";
+
+# Remove files and directory created by previous test
+my \$testingDir=\"\$toggle/dataTest/$module-noSGE-Blocks\";
+my \$cleaningCmd=\"rm -Rf \$testingDir\";
+system (\$cleaningCmd) and die (\"ERROR: \$0 : Cannot remove the previous test directory with the command \$cleaningCmd \\n\$!\\n\");
+
+#Creating config file for this test
+my \@listSoft = (\"$module\");
+fileConfigurator::createFileConf(\\\@listSoft,\"blockTestConfig.txt\");
+
+my \$runCmd = \"toggleGenerator.pl -c blockTestConfig.txt -d \".\$dataFastq.\" -r \".\$dataRefIrigin.\" -o \".\$testingDir;
+print \"\\n### Toggle running : \$runCmd\\n\";
+system(\"\$runCmd\") and die \"#### ERROR : Can't run TOGGLE for $module\";
+
+# check final results
+
+# expected output content
+my \$observedOutput = \`ls \$testingDir/finalResults\`;
+my \@observedOutput = split /\\n/,\$observedOutput;
+my \@expectedOutput = (\'FILE.EXTENTION\',\'FILE.EXTENTION\');
+
+is_deeply(\\\@observedOutput,\\\@expectedOutput,\'toggleGenerator - Two FILES (no SGE) $module::$function file list \');
+
+# expected output value TO DO
     
     ";
     
@@ -638,7 +628,9 @@ print "Finished...\n\n Please have a look to the following files to check if eve
     - modules/$moduleFile ##NOTE: Please check if the variable is noted as \$bwa and not /usr/bin/bwa !! 
     - modules/localConfig.pm
     - modules/softwareManagement.pm ##NOTE: Please check if the variable is noted as \$bwa and not /usr/bin/bwa !!
+    - modules/fileConfigurator.pm ##NOTE: Please add line $function =>[\"\"] to add default value for test block 
     - onTheFly/$blockName
-    - test/modules/$moduleTest\n";
+    - test/modules/$moduleTest
+    - test/blocks/$blockTest\n";
 
 exit;
